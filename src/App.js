5576e7ce0f2f969f9d54d9402e0d2b871322b16a
Gordon Ho <>
2019-01-15 10:57:18 +0800

    bug

--- src/App.js
+++ src/App.js
@@ -11,51 +11,65 @@ class App extends Component {
       term: "rgb",
       img: "",
       imgArray: [],
-      offset: 0,
+      offset: 0
     };
   }
 
-  onChange = (event) => {
+  onChange = event => {
     this.setState({ term: event.target.value });
-  }
+  };
 
-  handleSubmit = () => {
-    
+  handleSubmit = async() => {
     // if(event){event.preventDefault();}
     const api_key = "dc6zaTOxFJmzC";
     let offset = this.state.offset;
-    let url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}&limit=10&offset=${offset}`;
-    let imgArray = [];
-    axios(url)
-      .then(response => {
-        //console.log(data);
-        console.log(response.data.data);
-        response.data.data.map((item, index) => {
-          //console.log(offset + index);
-          // imgArray.push({img: { [Number(index + offset)]: item.images.fixed_height.url } });
-          //imgArray.push(item.images.fixed_height.url);
-          imgArray.push(item);
-          
-        }
-        );
-        this.setState({ imgArray: this.state.imgArray.concat(imgArray) })
-        
-      })
-      .catch(e => console.log('error', e));
+    let url = `http://api.giphy.com/v1/gifs/search?q=${
+      this.state.term
+    }&api_key=${api_key}&limit=10&offset=${offset}`;
 
-  }
+    //axios w/promise
+    // axios(url)
+    //   .then(response => {
+    //     console.log(response.data.data);
+    //     this.setState({ imgArray: this.state.imgArray.concat(response.data.data) });
+    //   })
+    //   .catch(e => console.log("error", e));
+    
+    //axios w/ await
+    try {
+      let response = await axios(url);
+      console.log(response.data.data);
+      this.setState({
+        imgArray: this.state.imgArray.concat(response.data.data)
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+    
+
+    } 
+
+  onClickSearch = () => {
+    this.handleSubmit();
+  };
 
   onClickMore = async () => {
-     this.setState({ offset: this.state.offset + 10 }, );
+    this.setState({ offset: this.state.offset + 10 });
     await this.handleSubmit();
-    }
+  };
 
   render() {
-    return <div className="App">
-        
-          <input value={this.state.term} onChange={this.onChange} />
-          <button onClick={this.handleSubmit}>Search!</button>
-        
+    return (
+      <div
+        className="App"
+        style={{
+          backgroundColor: "#18181C",
+          width: "100%",
+          minHeight: "100vh"
+        }}
+      >
+        <input value={this.state.term} onChange={this.onChange} />
+        <button onClick={() => this.onClickSearch()}>Search!</button>
         <div>
           <ul>
             {this.state.imgArray.length == 0 ? (
@@ -76,22 +90,23 @@ class App extends Component {
             <button onClick={() => this.onClickMore()}>More</button>
           )}
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
 const ImgRender = (props) => {
   let { imgArray, term, offset } = props;
-  //let iR = [];
-  //console.log(imgArray);
-  // imgArray.map((key, index) => {
-  //   iR[index] = <img src={imgArray[index].img[index]} height="200" alt={term} key={index} />;
-  // });
-  // return iR;
-  return imgArray.map((item, index) => 
+  return imgArray.map((item, index) => (
     <a href={item.images.original.url} target="blank" key={index}>
-      <img src={item.images.fixed_height.url} height="200" alt={term}  />
-    </a>);
+      <img
+        src={item.images.fixed_height.url}
+        height="200"
+        alt={term}
+        style={{ color:'#18181C'}}
+      />
+    </a>
+  ));
 }
 
 export default App;
