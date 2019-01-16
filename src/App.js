cd446ed52d01f5ea5b65a5569e40fe45399bab98
Gordon Ho <>
2019-01-16 10:16:38 +0800

    imported css and react-contextmenu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 // import onChange from "./onChange";
+import "./tachyons.min.css";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
 class App extends Component {
   constructor(props) {
